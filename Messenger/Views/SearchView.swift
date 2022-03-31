ebcf42fc2567ee72773cf706bace93817a11daef
ChrisPDelaney <60331530+ChrisPDelaney@users.noreply.github.com>
2022-03-31 13:41:00 -0400

    Add files via upload

---
+++ Messenger/Views/SearchView.swift
@@ -0,0 +1,129 @@
+//
+//  SearchView.swift
+//  Messenger
+//
+//  Created by Afraz Siddiqui on 4/17/21.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    
+    //Allows you to dismiss given presentation by using this property wrapper
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @EnvironmentObject var model: AppStateModel
+    @State var text: String = ""
+
+    @State var usernames: [String] = []
+    
+    @State var selected: [String] = []
+    
+    let completion: ((String) -> Void)
+
+    init(completion: @escaping ((String) -> Void)) {
+        self.completion = completion
+    }
+    
+    var body: some View {//test comment
+        VStack {
+            TextField("Username...", text: $text) //the text the user is typing in
+                .modifier(CustomField())
+
+            Button("Search") { //ensures that user is not just searching for whitespace
+                guard !text.trimmingCharacters(in: .whitespaces).isEmpty else {
+                    return
+                }
+
+                model.searchUsers(queryText: text) { usernames in
+                    self.usernames = usernames
+                }
+            }
+
+            List {
+                ForEach(usernames, id: \.self) { name in //JP
+                    /*HStack {
+                        Image("Chris")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                        
+                        Text("Christopher")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            self.isClicked.toggle()
+                        }) {
+                            Image(systemName: self.isClicked == true ? "checkmark.circle.fill" : "circle")
+                                .resizable()
+                                .foregroundColor(.blue)
+                                .padding()
+                                .frame(width: 75, height: 75)
+                        }
+                    }*/
+                    HStack {
+                        Image("photo1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 55, height: 55)
+                            .clipShape(Circle())
+
+                        Text(name) //JP
+                            .font(.system(size: 24))
+                                
+                        Spacer()
+                        
+                        Image(systemName: selected.contains(name) ? "checkmark.circle.fill" : "circle") //if array contains name (fill/empty)
+                            .resizable()
+                            .foregroundColor(.blue)
+                            .padding()
+                            .frame(width: 55, height: 55)
+                    } //open up
+                    .onTapGesture { // add/remove to selected array
+                        if let idx = selected.firstIndex(of: name) { // if in array
+                            selected.remove(at: idx)
+                        } else { // if not in array
+                            selected.append(name)
+                        }
+                    }
+                    /*.onTapGesture {
+                        presentationMode.wrappedValue.dismiss()//this will dismiss the search view
+                        completion(name) //date not name JP
+                    }*/
+                }
+            }
+            
+            Button(action: {
+                
+            }) {
+                Text("Add to Group")
+                    .fontWeight(.bold)
+                    .font(.largeTitle)
+                    .foregroundColor(selected.isEmpty ? Color.blue : Color.white)
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color.blue, lineWidth: 5)
+                            .frame(height: 50)
+                            .background(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(selected.isEmpty ? Color.white : Color.blue)
+                            )
+                    )
+            }
+
+            Spacer()
+        }   
+        .navigationTitle("Search")
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView() { _ in }
+            .preferredColorScheme(.dark)
+    }
+}

5a6b282554e36280a687b72dfa1892beed217fed
JP Mancini <jpmancini623@gmail.com>
2022-04-06 19:21:57 -0400

    pre user changes

---
+++ Messenger/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  GroupUser.swift
+//  Messenger
+//
+//  Created by JP Mancini on 4/6/22.
+//
+
+import Foundation
+
+//struct User: Hashable{
+//    let name: String
+//    let picture: Image
+//    let beerCount: Int
+//}
